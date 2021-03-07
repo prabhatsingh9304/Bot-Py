363dda4fe80e6e42b4731d04a1ddcd669a5c12cc
prabhatsingh9304 <74491841+prabhatsingh9304@users.noreply.github.com>
2021-03-07 19:20:54 +0530

    Update install_for_linux.sh

--- install_for_linux.sh
+++ install_for_linux.sh
@@ -3,6 +3,7 @@ echo "Installation Process will start after you enter your password ....."
 sudo apt-get update
 sudo apt-get --ignore-missing install -y python3 python3-pip python3-pyaudio
 pip3 install speechrecognition gtts pyttsx3 playsound selenium
+chmod +x chromedriver
 #mac user uncomment the following code
 #pip3 install PyObjc
 #brew install portaudio
