bd344155996db4380136c2e735ac6217177bea52
prabhatsingh9304 <74491841+prabhatsingh9304@users.noreply.github.com>
2021-03-07 17:21:40 +0530

    Update and rename install_pyaudio.bat to install_for_windows.bat

---
+++ install_for_windows.bat
@@ -0,0 +1,8 @@
+@echo off
+title Bot-Py
+color 0a
+echo "Installation is in Progress ,Please make sure you are connected to internet"
+python -m pip install --upgrade pip
+pip install speechrecognition gtts pyttsx3 playsound selenium
+pip install pipwin
+pipwin install pyaudio
--- install_pyaudio.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-python -m pip install --upgrade pip
-pip install speechrecognition gtts pyttsx3 playsound selenium
-pip install pipwin
-pipwin install pyaudio
